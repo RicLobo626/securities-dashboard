4734a3af12b9272e22779ba1e41d75f4d4073003
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-12-12 20:15:19 +0000

    Add theme and css baseline

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,17 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#003134",
+      contrastText: "#aec1c2",
+    },
+  },
+});
 
 const router = createRouter({ routeTree });
 
@@ -15,9 +26,13 @@ const rootElement = document.getElementById("root")!;
 
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
+
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </StrictMode>
   );
 }
