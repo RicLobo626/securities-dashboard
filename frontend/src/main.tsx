57001050789470b52082aa31787810351ec7d221
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-12-13 17:18:51 +0000

    Add global styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { CssBaseline, GlobalStyles, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
     background: {
-      default: "#aec1c2",
+      default: "#e2e8f0",
     },
     primary: {
       main: "#003134",
@@ -34,10 +34,21 @@ const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
 
+  const globalStyles = (
+    <GlobalStyles
+      styles={{
+        html: { height: "100%" },
+        body: { height: "100%" },
+        "#root": { height: "100%" },
+      }}
+    />
+  );
+
   root.render(
     <StrictMode>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        {globalStyles}
         <RouterProvider router={router} />
       </ThemeProvider>
     </StrictMode>
