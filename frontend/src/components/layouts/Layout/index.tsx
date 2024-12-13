7cb0676a1dc8dbd3a40196ff6bf38b086bf9a52b
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-12-13 18:27:25 +0000

    Update layout structure

--- frontend/src/components/layouts/Layout/index.tsx
+++ frontend/src/components/layouts/Layout/index.tsx
@@ -1,14 +1,15 @@
+import { Box, Container } from "@mui/material";
 import { TheHeader } from "./TheHeader";
 import { Outlet } from "@tanstack/react-router";
 
 export const Layout = () => {
   return (
-    <>
+    <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
       <TheHeader />
 
-      <main>
+      <Container sx={{ padding: "2rem", height: "100%" }} maxWidth="lg">
         <Outlet />
-      </main>
-    </>
+      </Container>
+    </Box>
   );
 };
