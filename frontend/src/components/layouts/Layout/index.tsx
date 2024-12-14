a1b8f723f827f67705896cb71edc6b52ab9205ae
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-12-14 20:09:03 +0000

    Update layout container styles

--- frontend/src/components/layouts/Layout/index.tsx
+++ frontend/src/components/layouts/Layout/index.tsx
@@ -13,10 +13,14 @@ export const Layout = () => {
   };
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "column" }}>
+    <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
       <TheHeader />
 
-      <Container sx={{ padding: "2rem" }} maxWidth="lg">
+      <Container
+        sx={{ padding: { xs: "0.4rem", sm: "1rem", md: "2rem" }, flex: "1 1 100%" }}
+        maxWidth="lg"
+        disableGutters
+      >
         <CatchBoundary
           getResetKey={() => resetKey}
           errorComponent={() => <ErrorFallback onReset={handleReset} />}
